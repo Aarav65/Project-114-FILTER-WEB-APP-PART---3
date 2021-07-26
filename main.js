522329435741a15ac969c3cd60d532129f1b6f44
Aarav65 <76677576+Aarav65@users.noreply.github.com>
2021-07-26 20:21:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+
+noseX = 0;
+noseY = 0;
+
+function preload()
+{
+    mustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png")
+}
+
+function setup()
+{
+    canvas = createCanvas(290, 290);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(290, 290);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoded()
+{
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x - 40;
+        noseY = results[0].pose.nose.y + 3;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
+    image(mustache, noseX, noseY, 90, 48);
+}
+
+function take_snapshot()
+{
+    save('my_filter_lipstick_image.png');
+}
